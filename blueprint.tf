c3fd71d8cc06087f105db46ffde32600ca867dc0
Lionel Hercot <lhercot@cisco.com>
2025-02-12 08:15:33 +0100

    Add nodes and connections

--- blueprint.tf
+++ blueprint.tf
@@ -7,3 +7,41 @@ resource "hyperfabric_fabric" "fabric1" {
   description = "This fabric is created by terraform"
 }
 
+resource "hyperfabric_node" "node1" {
+  fabric_id  = hyperfabric_fabric.fabric1.id
+  name       = "leaf1"
+  model_name = var.leaf_model_name
+  roles      = ["LEAF"]
+}
+
+resource "hyperfabric_node" "node2" {
+  fabric_id  = hyperfabric_fabric.fabric1.id
+  name       = "leaf2"
+  model_name = var.leaf_model_name
+  roles      = ["LEAF"]
+}
+
+resource "hyperfabric_connection" "node1_node2_31" {
+  fabric_id = hyperfabric_fabric.fabric1.id
+  local = {
+    node_id   = hyperfabric_node.node1.node_id
+    port_name = var.connection_interface_1
+  }
+  remote = {
+    node_id   = hyperfabric_node.node2.node_id
+    port_name = var.connection_interface_1
+  }
+}
+
+resource "hyperfabric_connection" "node1_node2_32" {
+  fabric_id = hyperfabric_fabric.fabric1.id
+  local = {
+    node_id   = hyperfabric_node.node1.node_id
+    port_name = var.connection_interface_2
+  }
+  remote = {
+    node_id   = hyperfabric_node.node2.node_id
+    port_name = var.connection_interface_2
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -8,3 +8,22 @@ variable "fabric_name_prefix" {
   type        = string
   default = "DEVWKS1440-"
 }
+
+variable "leaf_model_name" {
+  description = "Name of the Model of Leaf"
+  type        = string
+  default     = "HF6100-32D"
+}
+
+variable "connection_interface_1" {
+  description = "Interface name for the first connection between the two nodes"
+  type        = string
+  default     = "Ethernet1_31"
+}
+
+variable "connection_interface_2" {
+  description = "Interface name for the second connection between the two nodes"
+  type        = string
+  default     = "Ethernet1_32"
+}
+
